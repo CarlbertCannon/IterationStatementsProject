80265c08dd89b05fc72edcdf11173941c33c83b4
CarlbertCannon <providenceneedschange@gmail.com>
2021-07-18 18:57:30 -0600

    modified code

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -8,27 +8,29 @@ namespace IterationStatements
         static void Main(string[] args)
         {
                 var numbers = new List<int>();
-                var num = 0;
+                var myNumber = 0;
             do
             {
-                num++;
-                numbers.Add(num);
-            } while (num < 100);
+                myNumber++;
+                numbers.Add(myNumber);
+            } while (myNumber < 100);
 
-            while (num < 200) ;
+            while (myNumber < 200)
             {
-                num++;
-                numbers.Add(num);
+                myNumber++;
+                numbers.Add(myNumber);
             }
+
             Console.WriteLine("Increase:");
 
-            foreach(var collection in numbers)
+            foreach(var item in numbers)
             {
-                Console.WriteLine(collection);
-            Console.WriteLine("Decrease:");
+                Console.WriteLine(item);            
             }
 
-            for (int i = 199; i <= numbers.Count && i >= 0; i--)     
+            Console.WriteLine("Decrease:");
+
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 Console.WriteLine($"{numbers[i]}");
             }
